ac6772d08cdf005c78524daf5905ceeb192c3951
Михаил Мыслицкий <mysmster@yandex.ru>
2022-12-10 14:09:39 +0300

    Add MeasurementUnits, Tag tables

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register, site
 
-# Register your models here.
+from .models import MeasurementUnits, Tag
+
+site.site_header = 'Администрирование Foodgram'
+EMPTY_VALUE_DISPLAY = 'Значение не указано'
+
+
+@register(MeasurementUnits)
+class IngredientAdmin(ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empy_value_display = EMPTY_VALUE_DISPLAY
+
+
+@register(Tag)
+class TagAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('name', 'slug')
+    empy_value_display = EMPTY_VALUE_DISPLAY
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+MAX_LEN_RECIPES = 255
+
+User = get_user_model()
+
+
+class MeasurementUnits(models.Model):
+    """ Единицы измерения"""
+
+    name = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=MAX_LEN_RECIPES,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Единица измерения'
+        verbose_name_plural = 'Единицы измерения'
+        ordering = ('name',)
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Название тега',
+        max_length=MAX_LEN_RECIPES,
+        unique=True,
+    )
+    color = models.CharField(
+        verbose_name='Цветовой HEX-код',
+        max_length=7,
+        default='#00ff7f',
+        null=True,
+        blank=True,
+        unique=True,
+    )
+    slug = models.SlugField(
+        'Slug тега',
+        max_length=MAX_LEN_RECIPES,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
