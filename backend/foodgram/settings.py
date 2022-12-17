d71525b84b7735fd42f5ab7d91d3b321b25a25f5
Михаил Мыслицкий <mysmster@yandex.ru>
2022-12-17 13:33:01 +0300

    Add user model, reorganize recipe models

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -115,21 +116,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+MAX_LEN_USER = 255
+
+
+class User(AbstractUser):
+    """Пользователи"""
+
+    username = models.CharField(
+        verbose_name="Логин",
+        max_length=MAX_LEN_USER,
+        unique=True,
+    )
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=MAX_LEN_USER,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name="Имя",
+        max_length=MAX_LEN_USER,
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия",
+        max_length=MAX_LEN_USER,
+    )
+    subscribe = models.ManyToManyField(
+        verbose_name='Подписка',
+        related_name='subscribers',
+        to='self',
+        symmetrical=False,
+    )
+
+    class Meta:
+        ordering = ("username",)
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"{self.username}, {self.email}"
